062b0aa6bc3109c86920331237a773e1fd57969b
paranoiaoops <imathinker.oops@gmail.com>
2019-12-25 05:43:43 +0900

    相対パスに変更

--- service-worker.js
+++ service-worker.js
@@ -8,9 +8,9 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('v1').then((cache) => {
             return cache.addAll([
-                './index.html',
-                './index.js',
-                './index.css',
+                '/index.html',
+                '/index.js',
+                '/index.css',
             ]);
         })
     );
